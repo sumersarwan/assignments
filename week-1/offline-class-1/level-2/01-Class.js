63fb10ec552342e9f3ad452391d5fa5e9a66845d
sumersarwan <119452205+sumersarwan@users.noreply.github.com>
2023-12-08 17:10:01 +0530

    week1.5

--- week-1/offline-class-1/level-2/01-Class.js
+++ week-1/offline-class-1/level-2/01-Class.js
@@ -1,12 +1,12 @@
-
 class Animal {
   constructor(name, legCount) {
-    this.name = name
-    this.legCount = legCount
+    this.name = name;
+    this.legCount = legCount;
+  }
+  static myType() {
+    console.log("Animal"); //method directly on the class
   }
   describe() {
-    return `${this.name} has ${this.legCount} legs`
+    return `${this.name} has ${this.legCount} legs`;
   }
 }
-
-
